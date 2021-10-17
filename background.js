b07dc64e5163e181bdd2178d6a4a24763df17427
Somajit <dey.somajit@gmail.com>
2021-10-17 09:05:21 +0530

    Added ?: in ()

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const resolver = "https://cf-ipfs.com/ipns/";
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-         return {redirectUrl: details.url.replace(/^(https?:\/\/)([^/]+\/ipns\/)?/, resolver)};
+         return {redirectUrl: details.url.replace(/^(https?:\/\/)(?:[^/]+\/ipns\/)?/, resolver)};
     },
     {
         urls: [
